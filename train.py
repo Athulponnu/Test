a0cff352f4b64eae4973269fcdf379ce02cf8916
Athul <152899418+Athulponnu@users.noreply.github.com>
2026-03-18 15:39:34 +0530

    inintal

---
+++ train.py
@@ -0,0 +1,15 @@
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+import joblib
+
+df = pd.read_csv("students.csv")
+
+X = df[["cgpa","internships"]]
+y = df["placed"]
+
+model = RandomForestClassifier()
+model.fit(X, y)
+
+joblib.dump(model, "model.pkl")
+
+print("Training done")
